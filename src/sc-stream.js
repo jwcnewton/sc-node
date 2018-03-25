f1f54914f457c97d21306f1290bebe7f747c2c24
jwcnewton <jwcnewton@gmail.com>
2018-03-25 16:34:53 +0100

    Added event emit on track info undefined or not array

--- src/sc-stream.js
+++ src/sc-stream.js
@@ -5,15 +5,19 @@ const sc_tags = require('./sc-tags');
 const sc_events = require('./utils/sc-events');
 
 const downloadStreams = (trackInfos, client_id) => {
-    sc_events.emit('progress', `Downloading ${trackInfos.length} tracks`);
-    const dlArry = [];
-    for (let i = 0; i < trackInfos.length; i++) {
-        const trackInfo = trackInfos[i];
-        dlArry.push(downloadStream(trackInfo, client_id))
+    if(!trackInfos || typeof trackInfos === "array") {
+        sc_events.emit('progress', `No tracks to download`);
+    } else {
+        sc_events.emit('progress', `Downloading ${trackInfos.length} tracks`);
+        const dlArry = [];
+        for (let i = 0; i < trackInfos.length; i++) {
+            const trackInfo = trackInfos[i];
+            dlArry.push(downloadStream(trackInfo, client_id))
+        }
+        return Promise.all(dlArry).then(function(){
+            sc_events.emit('progress', 'All Done!');
+        });
     }
-    return Promise.all(dlArry).then(function(){
-        sc_events.emit('progress', 'All Done!');
-    });
 }
 
 const downloadStream = (trackInfo, client_id) => {
