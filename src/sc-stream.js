b4ba41597ffd41be2ab6a145f39d185f267555f7
jwcnewton <jwcnewton@gmail.com>
2018-06-05 13:49:21 +0100

    Added new event and output path option

--- src/sc-stream.js
+++ src/sc-stream.js
@@ -3,11 +3,15 @@ const sanitize = require("sanitize-filename");
 const fs = require('fs');
 const sc_tags = require('./sc-tags');
 const sc_events = require('./utils/sc-events');
+var outputfolder = "./";
 
-const downloadStreams = (trackInfos, client_id) => {
+const downloadStreams = (trackInfos, client_id, outputFolder) => {
     if(!trackInfos || typeof trackInfos === "array") {
         sc_events.emit('progress', `No tracks to download`);
     } else {
+        if(outputFolder){
+            outputfolder = outputfolder;
+        }
         sc_events.emit('progress', `Downloading ${trackInfos.length} tracks`);
         const dlArry = [];
         for (let i = 0; i < trackInfos.length; i++) {
@@ -26,11 +30,14 @@ const downloadStream = (trackInfo, client_id) => {
         const filename = sanitize(trackInfo.title);
 
         const stream = request(`${trackUri}?client_id=${client_id}`)
-            .pipe(fs.createWriteStream(`${filename}.mp3`))
-
+            .pipe(fs.createWriteStream(`${outputfolder}${filename}.mp3`));
+        
         stream.on('finish', () => {
             sc_events.emit('progress', `Pulled track: ${filename}`);
-            sc_tags.appendTags(stream.path, trackInfo).then(fulfill)
+            sc_tags.appendTags(stream.path, trackInfo).then(() => {
+                sc_events.emit('count', `Tagged`);
+                fulfill();
+            });
         });
         stream.on('error', reject);
     });
